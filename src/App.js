27b9b98502ade5a01a8cdd783bab251418a30b4d
letiescanciano <leticia.escanciano@gmail.com>
2021-05-03 12:40:34 +0200

    session 3

--- src/App.js
+++ src/App.js
@@ -1,153 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import styled from "styled-components";
 import "./App.css";
 
 import { NavBar } from "./components/NavBar";
-import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
-import EditIcon from "@material-ui/icons/Edit";
-const data = [
-  {
-    id: 0,
-    date: "22/04/2021",
-    name: "Coffee",
-    category: "eating_out",
-    amount: 4.5,
-    type: "expense",
-  },
-  {
-    id: 1,
-    date: "22/04/2021",
-    name: "Coffee",
-    category: "eating_out",
-    amount: 4.5,
-    type: "expense",
-  },
-  {
-    id: 2,
-    date: "22/04/2021",
+import { TransactionsList } from "./components/Transactions/List";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
-    name: "April payroll",
-    category: "salary",
-    amount: 4500,
-    type: "income",
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#FF7661",
+      contrastText: "#fff",
+    },
+    text: {
+      primary: "#fff",
+    },
   },
-  {
-    id: 3,
-    date: "22/04/2021",
-
-    name: "Coffee",
-    category: "eating_out",
-    amount: 4.5,
-    type: "expense",
+  overrides: {
+    MuiInputLabel: {
+      root: { color: "#fff", fontWeight: "normal" },
+    },
+    MuiInput: {
+      underline: {
+        "&:before": {
+          borderBottom: "1px solid #FF7661",
+        },
+        "&:hover": {
+          borderBottom: "1px solid #FF7661",
+        },
+      },
+    },
+    MuiFormLabel: {
+      root: {
+        color: "#fff",
+        fontWeight: 600,
+        paddingBottom: "16px",
+      },
+    },
+    MuiRadio: {
+      root: {
+        color: "#fff",
+      },
+    },
   },
-  {
-    id: 4,
-    date: "22/04/2021",
-    name: "Coffee",
-    category: "eating_out",
-    amount: 4.5,
-    type: "expense",
-  },
-  {
-    id: 5,
-    date: "22/04/2021",
-    name: "Coffee",
-    category: "eating_out",
-    amount: 4.5,
-    type: "expense",
-  },
-];
-
-const Table = styled.table`
-  width: 80%;
-  text-align: left;
-  padding: 64px;
-`;
-
-const HeadCell = styled.th`
-  padding: 16px 0;
-  width: 20%;
-`;
-const TableCell = styled.td`
-  padding: 8px 0;
-  width: 20%;
-`;
-const Amount = styled.p`
-  color: ${({ type }) => (type === "expense" ? "#FF7661" : "#00E4C6")};
-`;
+});
 
 function App() {
-  const [transactions, setTransactions] = useState([]);
-  const [loading, setLoading] = useState();
-
-  useEffect(() => {
-    setTimeout(() => {
-      setTransactions(data);
-    }, 2200);
-  }, []);
-  const formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  });
-
-  const handleDelete = (id) => {
-    console.log("id", id);
-    const _transactions = [...transactions].filter(
-      (transaction) => transaction.id !== id
-    );
-    setTransactions(_transactions);
-  };
   return (
-    <div className='layout'>
-      <NavBar />
-      <Table>
-        <thead>
-          <tr>
-            <HeadCell>Date</HeadCell>
-            <HeadCell>Name</HeadCell>
-            <HeadCell>Category</HeadCell>
-            <HeadCell>Amount</HeadCell>
-            <HeadCell></HeadCell>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions.length ? (
-            transactions.map((transaction) => {
-              return (
-                <tr key={transaction.id}>
-                  <TableCell>{transaction.date}</TableCell>
-                  <TableCell>{transaction.name}</TableCell>
-                  <TableCell>{transaction.category}</TableCell>
-                  <TableCell>
-                    <Amount type={transaction.type}>
-                      {formatter.format(transaction.amount)}
-                    </Amount>
-                  </TableCell>
-                  <TableCell>
-                    <EditIcon
-                      style={{ marginRight: "16px" }}
-                      onClick={(e) => {
-                        console.log("I should open a modal");
-                        console.log(e, transaction.id);
-                      }}
-                    />
-                    <DeleteForeverIcon
-                      style={{ color: "#FF7661" }}
-                      onClick={() => handleDelete(transaction.id)}
-                      // onClick={(e) => console.log(e, transaction.id)}
-                    />
-                  </TableCell>
-                </tr>
-              );
-            })
-          ) : (
-            <h3>Loading...</h3>
-          )}
-        </tbody>
-      </Table>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className='layout'>
+        <NavBar />
+        <TransactionsList />
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/components/Transactions/List/index.jsx
+++ src/components/Transactions/List/index.jsx
@@ -1,38 +1,311 @@
-import React from "react";
-import styled, { css } from "styled-components";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import * as Yup from "yup";
+import { Formik, Form } from "formik";
 
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import EditIcon from "@material-ui/icons/Edit";
+import Button from "@material-ui/core/Button";
+import Drawer from "@material-ui/core/Drawer";
+import TextField from "@material-ui/core/TextField";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+
+import data from "./data";
+import { Debug } from "../../../aux/Debug";
 const Table = styled.table`
-  width: 80%;
+  width: 100%;
   text-align: left;
-  padding: 8px;
+  padding: 16px 0;
 `;
 
-const TransactionsList = ({ transactions }) => {
+const HeadCell = styled.th`
+  padding: 16px 0;
+  width: 20%;
+`;
+const TableCell = styled.td`
+  padding: 8px 0;
+  width: 23%;
+  &(:last-of-type) {
+    display: flex;
+    justify-content: flex-end;
+    width: 8%;
+  }
+`;
+const Amount = styled.p`
+  color: ${({ type }) => (type === "expense" ? "#FF7661" : "#00E4C6")};
+`;
+
+const Grid = styled.div`
+  width: 100%;
+  padding: 64px;
+`;
+
+const AddButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Container = styled.div`
+  padding: 16px;
+  width: 380px;
+  height: 100vh;
+  overflow: scroll;
+  background-color: #252f3d;
+  color: white;
+`;
+const RadioOptionsWrapper = styled.div`
+  display: flex;
+  padding-top: 24px;
+`;
+
+const FieldsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const ActionsWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+const categories = [
+  { value: "eating_out", label: "Eating out" },
+  { value: "clothes", label: "Clothes" },
+  { value: "electronics", label: "Electronics" },
+  { value: "groceries", label: "Groceries" },
+  { value: "other", label: "Other" },
+];
+
+const types = [
+  { value: "expense", label: "Expense" },
+  { value: "income", label: "Income" },
+];
+const TransactionsList = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  useEffect(() => {
+    setTransactions(data);
+  }, []);
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  });
+
+  const handleDelete = (id) => {
+    const _transactions = [...transactions].filter(
+      (transaction) => transaction.id !== id
+    );
+    setTransactions(_transactions);
+  };
+
+  const addTransaction = (data) => {
+    console.log("Data", data);
+    setTransactions([...transactions, { ...data }]);
+  };
+  const transactionSchema = Yup.object().shape({
+    name: Yup.string().required("Required field"),
+    date: Yup.string().required("Required field"),
+    amount: Yup.number().required("Required field"),
+    category: Yup.string().required("Required field"),
+    type: Yup.string().required("Required field"),
+  });
+
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Name</th>
-          <th>Category</th>
-          <th>Amount</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactions.map((transaction) => {
-          return (
-            <tr data-id={transaction.id}>
-              <td>{transaction.date}</td>
-              <td>{transaction.name}</td>
-              <td>{transaction.category}</td>
-              <td>{transaction.amount}</td>
-              <td>...</td>
+    <Grid>
+      <AddButtonWrapper>
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={() => setOpenDrawer(true)}
+        >
+          + Add transaction
+        </Button>
+      </AddButtonWrapper>
+      <Table>
+        <thead>
+          <tr>
+            <HeadCell>Date</HeadCell>
+            <HeadCell>Name</HeadCell>
+            <HeadCell>Category</HeadCell>
+            <HeadCell>Amount</HeadCell>
+            <HeadCell></HeadCell>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.length ? (
+            transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <TableCell>{transaction.date}</TableCell>
+                  <TableCell>{transaction.name}</TableCell>
+                  <TableCell>{transaction.category}</TableCell>
+                  <TableCell>
+                    <Amount type={transaction.type}>
+                      {formatter.format(transaction.amount)}
+                    </Amount>
+                  </TableCell>
+                  <TableCell>
+                    <EditIcon
+                      style={{ marginRight: "16px" }}
+                      onClick={(e) => {
+                        console.log("I should open edit mode");
+                        console.log(e, transaction.id);
+                      }}
+                    />
+                    <DeleteForeverIcon
+                      style={{ color: "#FF7661" }}
+                      onClick={() => handleDelete(transaction.id)}
+                    />
+                  </TableCell>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td>
+                <p>Loading...</p>
+              </td>
             </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+          )}
+        </tbody>
+      </Table>
+
+      {openDrawer && (
+        <Drawer
+          anchor='right'
+          open={true}
+          onClose={() => setOpenDrawer(!openDrawer)}
+        >
+          <Container>
+            <h2>New transaction</h2>
+            <Formik
+              initialValues={{
+                name: "",
+                amount: "",
+                date: "",
+                category: "eating_out",
+                type: "expense",
+              }}
+              validationSchema={transactionSchema}
+              onSubmit={(values, { setSubmitting }) => {
+                console.log(JSON.stringify(values));
+                addTransaction(values);
+                setSubmitting(false);
+                setOpenDrawer(!openDrawer);
+              }}
+            >
+              {({
+                isSubmitting,
+                isValid,
+                values,
+                handleChange,
+                touched,
+                errors,
+              }) => (
+                <>
+                  <Form>
+                    <FieldsWrapper>
+                      <TextField
+                        fullWidth
+                        id='name'
+                        name='name'
+                        label='Name'
+                        value={values.email}
+                        onChange={handleChange}
+                        error={touched.email && Boolean(errors.email)}
+                        helperText={touched.email && errors.email}
+                      />
+                      <TextField
+                        fullWidth
+                        id='date'
+                        name='date'
+                        label='Date'
+                        value={values.date}
+                        onChange={handleChange}
+                        error={touched.date && Boolean(errors.date)}
+                        helperText={touched.date && errors.date}
+                        // component={DatePicker}
+                      />
+                      <TextField
+                        id='amount'
+                        type='number'
+                        name='amount'
+                        label='Amount'
+                        value={values.amount}
+                        onChange={handleChange}
+                        error={touched.amount && Boolean(errors.amount)}
+                        helperText={touched.amount && errors.amount}
+                      />
+                      <RadioOptionsWrapper>
+                        <FormControl component='fieldset'>
+                          <FormLabel component='legend'>Category</FormLabel>
+                          <RadioGroup
+                            aria-label='category'
+                            name='category'
+                            value={values.category}
+                            onChange={handleChange}
+                          >
+                            {categories.map((category) => (
+                              <FormControlLabel
+                                value={category.value}
+                                control={<Radio color='primary' />}
+                                label={category.label}
+                              />
+                            ))}
+                          </RadioGroup>
+                        </FormControl>
+                        <FormControl component='fieldset'>
+                          <FormLabel component='legend'>Type</FormLabel>
+                          <RadioGroup
+                            aria-label='type'
+                            name='type'
+                            value={values.type}
+                            onChange={handleChange}
+                          >
+                            {types.map((type) => (
+                              <FormControlLabel
+                                value={type.value}
+                                control={<Radio color='primary' />}
+                                label={type.label}
+                              />
+                            ))}
+                          </RadioGroup>
+                        </FormControl>
+                      </RadioOptionsWrapper>
+                      <ActionsWrapper>
+                        <Button
+                          variant='outlined'
+                          color='primary'
+                          onClick={() => setOpenDrawer(false)}
+                        >
+                          Cancel
+                        </Button>
+                        <Button
+                          variant='contained'
+                          color='primary'
+                          type='submit'
+                          disabled={!isValid || isSubmitting}
+                        >
+                          Save
+                        </Button>
+                      </ActionsWrapper>
+                    </FieldsWrapper>
+                  </Form>
+                  <Debug />
+                </>
+              )}
+            </Formik>
+          </Container>
+        </Drawer>
+      )}
+    </Grid>
   );
 };
 
